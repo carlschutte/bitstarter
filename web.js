53ba0c3b4e0a965ff67c0e430e2747c938f408c6
Carl Schutte <carl.schutte@gmail.com>
2013-08-28 11:10:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+}));
 });
 
 var port = process.env.PORT || 5000;
