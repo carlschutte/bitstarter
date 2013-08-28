f8fccf047c19f1c11d03a97fbf4ccd33dbbf0925
Carl Schutte <carl.schutte@gmail.com>
2013-08-28 11:19:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,10 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-}));
+  response.send(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
